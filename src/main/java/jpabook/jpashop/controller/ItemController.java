3599e5d4fefc414f130106390d82091764b9e5ed
naegonggae <tkdtkd97@naver.com>
2023-06-21 22:34:13 +0900

    :memo: 변경 감지와 병합(merge)

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -64,15 +64,28 @@ public class ItemController {
 	@PostMapping("/items/{itemId}/edit")
 	public String updateItem(@ModelAttribute("form") BookForm form, @PathVariable Long itemId) {
 
-		Book book = new Book();
-		book.setId(form.getId()); // id 에 접근할 수 있는지 권한 체크가 필요하다
-		book.setName(form.getName());
-		book.setPrice(form.getPrice());
-		book.setStockQuantity(form.getStockQuantity());
-		book.setAuthor(form.getAuthor());
-		book.setIsbn(form.getIsbn());
+		// 이것들은 실제로 DB에 존재하는 아이디로 Book 객체를 하나 더 만든것이다.
+		// 존재하는 아이디로 객체를 만들었으니 JPA 가 commit 하기전에 변경감지를해서 DB의 아이디 객체에 update 를 해주나? 아니다.
+		// 지금 만든 Book 객체는 JPA 가 인지하지 못하는 영속상태에 있지 않기 때문이다.
+		// 그래서 변경감지를 하게끔 시키려면 새로만든 Book 객체를 영속상태로 만들어줘야한다. 방법은 find 로 찾아오고 나머지는 form 값을 입혀준다.
+//		Book book = new Book();
+//		book.setId(form.getId()); // id 에 접근할 수 있는지 권한 체크가 필요하다
+//		book.setName(form.getName());
+//		book.setPrice(form.getPrice());
+//		book.setStockQuantity(form.getStockQuantity());
+//		book.setAuthor(form.getAuthor());
+//		book.setIsbn(form.getIsbn());
+//		itemService.saveItem(book);
 
-		itemService.saveItem(book);
+		// 병합을 쓰지말아야하는 이유
+		// 병합은 변경감지와 다르게 통으로 수정하는 개념이다.
+		// 그래서 만약 가격은 한번 정하면 불변이다해서 수정할때 제외를 해주고 하다보면 null 값이 들어가게 된다...
+		// 병합으로 실무에서 깔끔하게 갈아끼우기 쉽지않다.
+		// 그래서 변경감지가 좋다.
+
+		// 세개만 바꾼다고 가정
+		itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+
 		return "redirect:/items";
 	}
 
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 import java.util.List;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repositiry.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,17 @@ public class ItemService {
 	@Transactional
 	public void saveItem(Item item) {
 		itemRepository.save(item);
+	}
+
+	@Transactional
+	// 파라미터가 너무 많으면 DTO 생성해서 하셈
+	public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+		Item findItem = itemRepository.findOne(itemId); // 영속상태에 불러옴
+		// setter 실무에서는 다 치워야함
+		findItem.setName(name);
+		findItem.setPrice(price);
+		findItem.setStockQuantity(stockQuantity);
+		// 하고 끝내면 변경감지를 해서 update 쿼리를 날린다.
 	}
 
 	public List<Item> findItems() {
