bd18d64926481791ad7169bb57880cf235cc44af
naegonggae <tkdtkd97@naver.com>
2023-06-21 10:56:29 +0900

    :memo: 상품 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.service;
+
+import java.util.List;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repositiry.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+	private final ItemRepository itemRepository;
+
+	@Transactional
+	public void saveItem(Item item) {
+		itemRepository.save(item);
+	}
+
+	public List<Item> findItems() {
+		return itemRepository.findAll();
+	}
+
+	public Item findOne(Long itemId) {
+		return itemRepository.findOne(itemId);
+	}
+}
