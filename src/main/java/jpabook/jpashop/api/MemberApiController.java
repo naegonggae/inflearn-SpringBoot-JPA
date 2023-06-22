49d74feebee36be7d4400655979d5552a7080928
naegonggae <tkdtkd97@naver.com>
2023-06-22 12:26:15 +0900

    :memo: 회원 수정 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,9 +3,12 @@ package jpabook.jpashop.api;
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,9 +29,26 @@ public class MemberApiController {
 	public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
 		Member member = new Member();
 		member.setUsername(request.getName());
-
+		System.out.println("0========");
 		Long id = memberService.join(member);
+		System.out.println("5========");
 		return new CreateMemberResponse(id);
+	}
+
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(
+			@PathVariable("id") Long id,
+			@RequestBody @Valid UpdateMemberRequest request) {
+
+		memberService.update(id, request.name); // 수정 로직에서 굳이 member 를 반환하지 않고 조회하는 메서드로 객체를 불러오자.
+		System.out.println("3========");
+		Member findMember = memberService.findOne(id);
+		System.out.println("4========");
+		return new UpdateMemberResponse(findMember.getId(), findMember.getUsername());
+
+		// update 메서드의 트랜잭션 어노테이션이 끝나는 지점은 어딜까...? -> 메서드 실행 종료
+		// update 메소드에서 select 하고 update 날리고 종료(변경감지로 commit 될때 알아서 update 한거고) -> 컨트롤러 와서 select 는 왜 안할까?
+
 	}
 
 	@Data
@@ -42,6 +62,18 @@ public class MemberApiController {
 
 	@Data
 	static class CreateMemberRequest {
+		private String name;
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse {
+		private Long id;
+		private String name;
+	}
+
+	@Data
+	static class UpdateMemberRequest {
 		private String name;
 	}
 }
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -20,16 +20,21 @@ public class MemberService {
 	@Transactional
 	public Long join(Member member) {
 		validateDuplicateMember(member);
+		System.out.println("3========");
 		memberRepository.save(member);
+		System.out.println("4========"); //이 메서드 끝나면 insert 쿼리
 		return member.getId();
 	}
 
 	private void validateDuplicateMember(Member member) {
+		System.out.println("1========"); // 다음에 select 쿼리
 		// 멀티 쓰레드 환경도 고려해서 member 의 name 에 유니크조건을 준다.
 		List<Member> findMember = memberRepository.findByName(member.getUsername());
 		if (!findMember.isEmpty()) { // 카운트해서 0보다 크면 에러 터트리면 성능 최적화 할 수 있다.
 			throw new IllegalStateException("이미 존재하는 회원입니다.");
 		}
+		System.out.println("2========");
+
 	}
 
 	// 전체 조회
@@ -42,4 +47,16 @@ public class MemberService {
 	}
 
 
+	@Transactional
+	public void update(Long id, String name) {
+		System.out.println("1========");
+
+		Member member = memberRepository.findOne(id);
+		member.setUsername(name);
+		System.out.println("2========");
+
+	}
+	// 커맨드와 쿼리는 분리 해야한다.
+	// member 를 반환하면 영속상태가 끊긴 객체가 반환된다.
+	// void 말고 member 로 리턴값을 주면 이 로직은 변경하는 쿼리를 날리지만 또 이걸 가져와서 조회하는 역할도 하게 된다.
 }
